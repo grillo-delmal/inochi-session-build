9e922b43ac7e3b77fb3991591d1f080f9e71daca
Grillo del Mal <grillo@delmal.cl>
2022-07-14 01:50:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Inochi Session builder
+
+This is my reference building process for inochi-session. I built this with the 
+purpose of using it as reference to eventually package the software into COPR.
+
+Versions of the software generated using this scripts are not supported by the 
+upstream project.
+
+If you use Fedora 36+ and don't mind using the software without support or the 
+branding assets, check out https://copr.fedorainfracloud.org/coprs/grillo-delmal/inochi2d/
+
+## Dependencies
+
+You just need podman to run this, should work fine with Docker too with some tweaks.
+
+Remember to initalize the repository submodules before continuing
+
+```sh
+git submodule update --init --recursive
+```
+
+## Build
+
+Just run the deploy script
+
+```sh
+./deploy.sh
+```
+
+The result should end up in the `build_out/inochi` folder
+
+## Run
+
+Go into the `build_out/inochi` folder and run the `./inochi-session` binary 
+or use the `run.sh` script.
+
+## Debug
+
+If you want to run a debug version of the application, run the `deploy.sh` 
+and `run.sh` scripts with the `DEBUG=1` env variable.
+
+```sh
+DEBUG=1 ./deploy.sh
+DEBUG=1 ./run.sh
+```
---
+++ scripts/build.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/bash
+
+set -e
+
+source /opt/build/semver.sh
+
+# Clean out folder
+find /opt/out/ -mindepth 1 -maxdepth 1 -exec rm -r -- {} +
+
+cd /opt
+mkdir src
+
+rsync -r /opt/orig/inochi-session/ /opt/src/inochi-session/
+
+rsync -r /opt/orig/inochi2d/ /opt/src/inochi2d/
+rsync -r /opt/orig/bindbc-imgui/ /opt/src/bindbc-imgui/
+rsync -r /opt/orig/gitver/ /opt/src/gitver/
+rsync -r /opt/orig/facetrack-d/ /opt/src/facetrack-d/
+rsync -r /opt/orig/fghj/ /opt/src/fghj/
+rsync -r /opt/orig/inmath/ /opt/src/inmath/
+rsync -r /opt/orig/inui/ /opt/src/inui/
+rsync -r /opt/orig/vmc-d/ /opt/src/vmc-d/
+rsync -r /opt/orig/i18n/ /opt/src/i18n/
+rsync -r /opt/orig/bindbc-spout2/ /opt/src/bindbc-spout2/
+
+# Add dlang deps
+dub add-local /opt/src/inochi2d/        "$(semver /opt/src/inochi2d/)"
+dub add-local /opt/src/gitver/          "$(semver /opt/src/gitver/)"
+dub add-local /opt/src/bindbc-imgui/    "$(semver /opt/src/bindbc-imgui/)"
+dub add-local /opt/src/facetrack-d/     "$(semver /opt/src/facetrack-d/)"
+dub add-local /opt/src/fghj/            "$(semver /opt/src/fghj/)"
+dub add-local /opt/src/inmath/          "$(semver /opt/src/inmath/)"
+dub add-local /opt/src/inui/            "$(semver /opt/src/inui/ 1.0.0)"
+dub add-local /opt/src/vmc-d/           "$(semver /opt/src/vmc-d/)"
+dub add-local /opt/src/i18n/            "$(semver /opt/src/i18n/)"
+dub add-local /opt/src/bindbc-spout2/    "$(semver /opt/src/bindbc-spout2/)"
+
+# Build bindbc-imgui deps
+pushd src
+pushd bindbc-imgui
+mkdir -p deps/build_linux_x64_cimguiDynamic
+
+if [[ -z ${DEBUG} ]]; then
+    cmake -S deps -B deps/build_linux_x64_cimguiDynamic
+    cmake --build deps/build_linux_x64_cimguiDynamic --config Release
+else
+    cmake -DCMAKE_BUILD_TYPE=Debug -S deps -B deps/build_linux_x64_cimguiDynamic
+    cmake --build deps/build_linux_x64_cimguiDynamic --config Debug
+fi
+
+popd
+popd
+
+# Build inochi-session
+pushd src
+pushd inochi-session
+if [[ ! -z ${DEBUG} ]]; then
+    export DFLAGS='-g --d-debug'
+fi
+dub build --config=barebones
+popd
+popd
+
+# Install
+rsync -r /opt/src/inochi-session/out/ /opt/out/inochi/
+
+dub list > /opt/out/version_dump
